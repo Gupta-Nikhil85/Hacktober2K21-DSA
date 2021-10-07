d84f328bdc49c00970571ba7e88fe30c1d47a9ba
ydv-manoj <mojirao770@gmail.com>
2021-10-07 20:57:36 +0530

    josephus using c++ stl(list)

---
+++ josephus2.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<list>
+using namespace std;
+int josephus(int n,int k){
+     list<int>l;
+	 for(int i=0;i<n;i++){
+		 l.push_back(i);
+	 }
+	 auto it =l.begin();
+	 while(l.size()>1){
+		 for(int count=1;count<k;count++){
+			 it++;
+			 if(it==l.end()){it=l.begin();}
+		 }
+		 it =l.erase(it);
+		 if(it==l.end()){it=l.begin()};
+		 return l.begin();
+	 }
+}
+
